a0067b8068ca2077122813ee3c13bd04c93299f9
HiznyakNikita <hiznyaknikita1996@gmail.com>
2017-05-09 11:04:16 +0300

    add BlockForSocialPool.Data.csproj
    add base Entity class and Interview model

---
+++ BlockForSocialPool/BlockForSocialPool.Data/Interview.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockForSocialPool.Data
+{
+    public enum InterviewType
+    {
+        Promo,
+        SociologicalResearch,
+        Service
+    }
+
+    public class Interview : Entity<Guid>
+    {
+        public virtual string Name { get; set; }
+        public virtual string Description { get; set; }
+        public virtual InterviewType Type { get; set; }
+        public virtual DateTime StartDate { get; set; }
+        public virtual DateTime DueDate { get; set; }
+        public virtual int MaxTriesOfSpending { get; set; }
+    }
+}
